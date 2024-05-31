e381827a8af8fb780652fcb6e3211e5c9b334bef
Azz0102 <nguyenduchuyhht2@gmail.com>
2024-05-31 16:49:24 +0700

    FEAT: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
+router.get("/checkstatus", (req, res, next) => {
     res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+        status: "success",
+        message: "api ok",
+    });
+});
+
+router.get("/api/users", (req, res, next) => {
+    res.status(200).json({
+        status: "success",
+        message: "api ok",
+        metadata: [
+            { name: "hator", age: 12 },
+            { name: "baycho", age: 30 },
+            { name: "rauta", age: 33 },
+        ],
+    });
+});
 
 module.exports = router;
